a754031982a16ea6590e32a66bd6ae8928881a2d
Luke <luke.nguyen@vmodev.com>
2023-07-25 17:48:15 +0700

    remove unused code

--- frontend-reactjs/src/components/home/homepage.component.tsx
+++ frontend-reactjs/src/components/home/homepage.component.tsx
@@ -5,18 +5,10 @@ import { useNavigate, useParams } from "react-router-dom";
 import FooterPage from "./footer.component";
 export const HomePage = () => {
 
-    const [count, setCount] = useState(0);
-
-    useEffect(() => {
-        document.title = `You clicked ${count} times`;
-    }, [count]);
-
     return (
         <html>
             <div className="home-page">
                 <h1 className="text-center">VMO Holdings</h1>
-
-                <h1>Welcome to my app!</h1>
                 <br />
             </div>
             <FooterPage/>
